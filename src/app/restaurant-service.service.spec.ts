7429bfadf4a2068a8b47ce3df0e501703c2f6b1e
Cpete040 <Cpete040@uottawa.ca>
2022-06-02 21:57:42 +0000

    [Fix] restaurant service httpClient test

--- src/app/restaurant-service.service.spec.ts
+++ src/app/restaurant-service.service.spec.ts
@@ -1,16 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
+import { Restaurant } from './restaurant';
 import { RestaurantService } from './restaurant-service.service';
 
 describe('RestaurantService', () => {
   let service: RestaurantService;
+  let httpMock: HttpTestingController;
+  let httpClient: HttpClient;
+  let restaurant: Restaurant;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [RestaurantService, HttpClient]
+    });
     service = TestBed.inject(RestaurantService);
-  });
+    httpMock = TestBed.inject(HttpTestingController);
+    httpClient = TestBed.inject(HttpClient);
 
-  it('should be created', () => {
     expect(service).toBeTruthy();
   });
 });
